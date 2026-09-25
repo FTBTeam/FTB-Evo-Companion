b3474ec914ff550cae72708dbf45b67452960e25
Saereth <paulanthonydevs@gmail.com>
2026-09-24 22:22:38 -0700

    model and texture refinement for the fabricator

--- src/main/java/dev/ftb/mods/ftbevolutioncompanion/client/FabricatorItemRenderer.java
+++ src/main/java/dev/ftb/mods/ftbevolutioncompanion/client/FabricatorItemRenderer.java
@@ -2,6 +2,8 @@ package dev.ftb.mods.ftbevolutioncompanion.client;
 
 import com.geckolib.model.DefaultedBlockGeoModel;
 import com.geckolib.renderer.GeoItemRenderer;
+import com.geckolib.renderer.base.GeoRenderState;
+import com.geckolib.renderer.base.RenderPassInfo;
 import com.geckolib.renderer.layer.builtin.AutoGlowingGeoLayer;
 import dev.ftb.mods.ftbevolutioncompanion.FTBEvolutionCompanion;
 import dev.ftb.mods.ftbevolutioncompanion.fabricator.FabricatorItem;
@@ -10,5 +12,12 @@ public final class FabricatorItemRenderer extends GeoItemRenderer<FabricatorItem
     public FabricatorItemRenderer() {
         super(new DefaultedBlockGeoModel<>(FTBEvolutionCompanion.id("ftb_fabricator")));
         withRenderLayer(new AutoGlowingGeoLayer<>(this));
+    }
+
+    @Override
+    public void adjustRenderPose(RenderPassInfo<GeoRenderState> pass) {
+        super.adjustRenderPose(pass);
+        // The block model spans Y=0..16; item transforms rotate around its center.
+        pass.poseStack().translate(0F, -0.5F, 0F);
     }
 }
